dced59e7ef9caf3ca6f3a70e0ce4247219b57adb
Gulchın Ramazanov <glchnrmznv@Gulchn-MacBook-Air.local>
2024-10-20 17:21:39 +0400

    Mesaj

---
+++ index.js
@@ -0,0 +1,10 @@
+var payment;
+(function (payment) {
+    payment["paypaL"] = "PAYPAL";
+    payment["PASHAPAY"] = "PASHAPAY";
+})(payment || (payment = {}));
+var Country;
+(function (Country) {
+    Country["country1"] = "Dominican Republic";
+    Country["country2"] = "Egypt";
+})(Country || (Country = {}));
